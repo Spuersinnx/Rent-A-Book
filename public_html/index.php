bcc46ccaf38fc34c0067859082d6e025d881b65c
May Moftah <maymoftah@outlook.com>
2017-03-08 16:07:45 -0500

    Created Index HTML with initial design and layout
    Signed-off-by: May Moftah <maymoftah@outlook.com>

---
+++ public_html/index.php
@@ -0,0 +1,67 @@
+<?php
+?>
+
+
+<html>
+<head>
+    <title>Rent a Book</title>
+    <link type="text/css" rel="stylesheet" href="css/mainstyle.css">
+
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<header>
+    <div class="wrapper">
+        <h1>Rent a Book<span class="color">.</span></h1>
+        <nav>
+            <ul>
+                <li><a href="#">Log In</a></li>
+                <li><a href="#">Sign Up</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+<div id="bookshelf-image">
+</div>
+
+<div id="perks">
+    <div class="wrapper">
+        <ul>
+            <li class="perk-1">
+                <h4>Easy to Join</h4>
+                <p>It is easy to join our book rental community. With a click of a button, you can become a member and you can rent books or even provide books for rental to others in the community.</p>
+            </li>
+            <li class="perk-2">
+                <h4>Community Focused</h4>
+                <p>Our community is made up of students, companies, professors and more. Members of Rent a Book can provide our use our services in a community-centric approach towards book rentals.</p>
+
+            </li>
+            <li class="perk-3">
+                <h4>Large Selection</h4>
+                <p>Our selection of books is dependent on our members and the books they provide. With our subscription rate increase, more and more books are being added to our collection everyday.</p>
+            </li>
+            <div class="clear"></div>
+        </ul>
+    </div>
+</div>
+
+
+<footer>
+    <div class="wrapper">
+        <div id="footer-info">
+            <p>Copyright 2014 Rent a Book. All rights reserved.</p>
+            <p><a href="#">Terms of Service</a> I <a href="#">Privacy</a></p>
+            <div>Icons made by <a href="http://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+        </div>
+    </div>
+</footer>
+
+
+
+
+</body>
+</html>
+
+
