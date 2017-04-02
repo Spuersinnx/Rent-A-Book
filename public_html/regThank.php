a36eded7c927cbfe262cf10f6b1880364a47cf51
May Moftah <maymoftah@outlook.com>
2017-04-01 21:05:47 -0400

    Changed regThank.php
    Signed-off-by: May Moftah <maymoftah@outlook.com>

--- public_html/regThank.php
+++ public_html/regThank.php
@@ -6,6 +6,44 @@
  * Time: 4:46 PM
  */
 
-echo 'Thanks :D ';
 
 ?>
+
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+    <title>Thank You</title>
+    <link type="text/css" rel="stylesheet" href="css/mainstyle.css">
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+</head>
+
+
+<body>
+<header>
+<div class="wrapper">
+
+<div id="message">
+<h4>Thank You For Joining Rent a Book</h4>
+</div>
+</div>
+</header>
+
+
+<p> Please check your mailbox and confirm your email to activate your Rent a Book membership!</p>
+
+
+
+</body>
+<footer>
+    <div class="wrapper">
+        <div id="footer-info">
+            <p><a href="#">Terms of Service</a> I <a href="#">Privacy</a></p>
+            <p>Icons made by <a href="http://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel
+                    perfect</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed
+                by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0"
+                      target="_blank">CC 3.0 BY</a></p>
+        </div>
+    </div>
+</footer>
+
+
+</html>
