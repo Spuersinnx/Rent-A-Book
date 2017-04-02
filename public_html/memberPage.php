e20eccc29000706abee948d0396a97958ab30b3d
May Moftah <maymoftah@outlook.com>
2017-04-01 20:52:14 -0400

    Added memberPage.php + started CSS changes
    Signed-off-by: May Moftah <maymoftah@outlook.com>

---
+++ public_html/memberPage.php
@@ -0,0 +1,55 @@
+<?php
+
+
+
+?>
+
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+    <title>Rent a Book</title>
+    <link type="text/css" rel="stylesheet" href="css/mainstyle.css">
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<header>
+    <div class="wrapper">
+        <h1>Rent a Book<span class="color">.</span></h1>
+        <nav>
+            <ul>
+                <li><button class="dropdownbtn">My Account</button></li>
+                <li><button class="dropdownbtn"><a href="index.php"> Log Out</a></button></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+<!--Main background Image-->
+<div id="books-image">
+    <h2>Welcome Back</h2>
+</div>
+
+<div id="menu-container">
+    <img src="img/content/user.png"><p>Username</p>
+
+
+
+</div>
+
+
+
+
+
+<footer>
+    <div class="wrapper">
+        <div id="footer-info">
+            <p><a href="#">Terms of Service</a> I <a href="#">Privacy</a></p>
+            <p>Icons made by <a href="http://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel
+                    perfect</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed
+                by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0"
+                      target="_blank">CC 3.0 BY</a></p>
+        </div>
+    </div>
+</footer>
+
+</html>
