43020ff877381977285e36d31f795c5539111889
jenn <epistemicpolymathic5@gmail.com>
2017-04-16 22:57:43 -0400

    Working on db_search.php trying to create a query that will display results from a single input search box field.
    Signed-off-by: jenn <epistemicpolymathic5@gmail.com>

--- db/db_search.php
+++ db/db_search.php
@@ -7,7 +7,22 @@ require_once("db_config.php");
 #Get Post variables from search form
 $userSearch = filter_input(INPUT_POST, "userSearch");
 
+#Changes characters in html to their equivalent forms
+#$userSearch = htmlspecialchars($userSearch);
 
 
 #Queries for searching the database for books
-$basicSearchQuery = $db->prepare("SELECT ");
+$basicSearchQuery = $db->prepare("SELECT *
+                                      FROM book b
+                                      WHERE  b.BookID IN (
+                                        SELECT books.BookID, author.authorName 
+                                            FROM books INNER JOIN author
+                                            ON ( books.bookID = author.bookID)
+                                            WHERE author.authorName LIKE '%:userSearch%')
+                                        OR b.ISBN LIKE '%:userSearch%'
+                                        OR b.bookname LIKE '%:userSearch%'");
+
+$basicSearchQuery->execute(array(
+    ":userSearch" => $userSearch
+));
+$userResults = $basicSearchQuery->fetchAll();
