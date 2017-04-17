63e97df0aa01a521a6009ba70dc5876b75e8fd10
jenn <epistemicpolymathic5@gmail.com>
2017-04-16 23:01:21 -0400

    Edited search.php slightly
    Signed-off-by: jenn <epistemicpolymathic5@gmail.com>

--- public_html/Search.php
+++ public_html/Search.php
@@ -9,27 +9,6 @@
     <link type="text/css" rel="stylesheet" href="css/mainstyle.css">
     <link type="text/css" rel="stylesheet" href="css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script>
-        /* $(document).ready(function () {
-         $('#search-button').click(function (e) {
-
-         e.preventDefault();
-
-         var bookName = $("[name='bookName']").val();
-         var ISBN = $("[name='ISBN']").val();
-
-
-         if (bookName == "" || bookName.length < 2 || bookName == " " || ISBN == " " || ISBN == "" || !(ISBN.length >= 10)) {
-         alert('There were errors encountered with your form, please review the fields');
-         return false;
-         }
-
-
-         }
-
-         }); */
-    </script>
-
 </head>
 
 <body>
